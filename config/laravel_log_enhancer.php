fccc0704a1af036e53495c69d384a0c7d5187cc2
Ashwani Goyal <ashwanigoyal@Sameers-Mac-mini-2.local>
2018-05-30 10:30:32 +0530

    Initial Commit

---
+++ config/laravel_log_enhancer.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'log_request_details' => true,
+	'log_input_data' => true,
+	'log_request_headers' => false,
+	'log_session_data' => true,
+	'log_memory_usage' => false,
+	'log_git_data' => false,
+	'ignore_input_fields' => ["password", "confirm_password"],
+];
